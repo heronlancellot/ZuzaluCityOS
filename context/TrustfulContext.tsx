60ca27cf82eeea2a3d02da8f0f70884faf1d8a13
heronlancellot <heronlancellot@id.uff.br>
2024-10-22 16:52:53 -0300

    feat: add trustful-context

---
+++ context/TrustfulContext.tsx
@@ -0,0 +1,65 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useMemo,
+  type ReactNode,
+  type Dispatch,
+  type SetStateAction,
+} from 'react';
+import { Address } from 'viem';
+
+export enum Role {
+  ROOT = 'ROOT',
+  MANAGER = 'MANAGER',
+  VILLAGER = 'VILLAGER',
+}
+
+interface User {
+  address: Address;
+  role: Role;
+}
+
+interface TrustfulContextType {
+  setUserRole: Dispatch<SetStateAction<User | null>>;
+  userRole: User | null;
+}
+
+const defaultContextValue: TrustfulContextType = {
+  userRole: null,
+  setUserRole: () => {},
+};
+
+const TrustfulContext = createContext<TrustfulContextType>(defaultContextValue);
+
+interface TrustfulContextProviderProps {
+  children: ReactNode;
+}
+
+export const TrustfulContextProvider: React.FC<
+  TrustfulContextProviderProps
+> = ({ children }) => {
+  const [userRole, setUserRole] = useState<User | null>(null);
+
+  const TrustfulContextData = useMemo(
+    () => ({
+      userRole,
+      setUserRole,
+    }),
+    [userRole],
+  );
+
+  return (
+    <TrustfulContext.Provider value={TrustfulContextData}>
+      {children}
+    </TrustfulContext.Provider>
+  );
+};
+
+export const useTrustful = (): TrustfulContextType => {
+  const context = useContext(TrustfulContext);
+  if (context === undefined) {
+    throw new Error('useTrustful must be used within a TrustfulContext');
+  }
+  return context;
+};
