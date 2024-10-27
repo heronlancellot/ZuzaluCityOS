c5423fef6d4eca1a4898d573ee96f722f8b49273
KieSun <zx597813039@gmail.com>
2024-10-27 09:48:40 +0800

    fix: add No Ticketing Required to componentsMap

--- components/event/EventRegister/modes/Zupass/index.tsx
+++ components/event/EventRegister/modes/Zupass/index.tsx
@@ -39,7 +39,6 @@ const ZupassDefault: React.FC<ZupassDefaultProps> = ({
   return (
     <Stack
       padding={'24px 20px'}
-      border={'1px solid rgba(255, 255, 255, 0.10)'}
       borderRadius={'0px 0px 10px 10px'}
       margin={'0px !important'}
       gap={'20px'}
